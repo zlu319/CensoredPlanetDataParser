afdd9730979661be945d660806e87b8eaf0d86f9
Colton Mack <colton.mack@gmail.com>
2022-04-21 00:20:50 -0400

    added counts

--- censored-lookup-fixed.py
+++ censored-lookup-fixed.py
@@ -10,10 +10,14 @@ output_dir = "censored-outputs"
  
 for filename in filenames:
     censored_output = dict()  # format = website: anomaly_count, pass_count, category, ip
+    censored_count = 0
+    total_count = 0
     print(filename)
     f = open(filename)
     json_object = json.load(f)
     for key, values in json_object.items():
+        censored_count += values[0]
+        total_count += (values[0] + values[1])
         if (values[1] != 0 and values[0]/values[1] > .2) or (values[1] == 0 and values[0] != 0):
             censored_output[key] = [values[2], values[3], "EMPT"]
             if values[3] != "0.0.0.0":
@@ -25,3 +29,5 @@ for filename in filenames:
     output_loc = os.path.join(output_dir, os.path.basename(filename))
     with open(output_loc, "w") as outfile:
         json.dump(censored_output, outfile, indent=2)
+        outfile.write("Censored Count: {}".format(censored_count))
+        outfile.write("Total Count: {}".format(total_count))
