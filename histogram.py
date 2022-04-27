c4c4c01157d103adcc25f65256e1a53f1c6ec387
Colton Mack <colton.mack@gmail.com>
2022-04-26 22:48:13 -0400

    histogram

---
+++ histogram.py
@@ -0,0 +1,24 @@
+import requests
+import glob
+import json
+import os
+import math 
+
+file_location = os.path.join('summary-with-ip', '*site_stats.json')
+filenames = glob.glob(file_location)
+req = "https://geolocation-db.com/json/d802faa0-10bd-11ec-b2fe-47a0872c6708/"
+output_dir = "histogram-outputs"
+ 
+for filename in filenames:
+    count = [0] * 20  # format = website: anomaly_count, pass_count, category, ip
+    print(filename)
+    f = open(filename)
+    json_object = json.load(f)
+    for key, values in json_object.items():
+        ratio = values[0]/(values[1]+values[0])
+        index = math.floor(ratio * 20)
+        count[index] += 1
+    temp = 0
+    for i in range(0,20):
+        print("{}, {}".format(temp, count[i]))
+        temp += .05
