91e55b8db6683c4cc9905223f20f88ee743520ca
zlu319 <93557381+zlu319@users.noreply.github.com>
2022-04-26 23:08:48 -0400

    Fixed histogram.py
    Fixed an Index Out Of Bounds error, also cleaned up output formatting.

--- histogram.py
+++ histogram.py
@@ -4,13 +4,14 @@ import json
 import os
 import math 
 
-file_location = os.path.join('summary-with-ip', '*site_stats.json')
+inputdir = 'summary-with-ip'
+file_location = os.path.join(inputdir, '*site_stats.json')
 filenames = glob.glob(file_location)
 req = "https://geolocation-db.com/json/d802faa0-10bd-11ec-b2fe-47a0872c6708/"
 output_dir = "histogram-outputs"
  
 for filename in filenames:
-    count = [0] * 20  # format = website: anomaly_count, pass_count, category, ip
+    count = [0] * 21  # format = website: anomaly_count, pass_count, category, ip
     print(filename)
     f = open(filename)
     json_object = json.load(f)
@@ -19,6 +20,6 @@ for filename in filenames:
         index = math.floor(ratio * 20)
         count[index] += 1
     temp = 0
-    for i in range(0,20):
-        print("{}, {}".format(temp, count[i]))
+    for i in range(0,21):
+        print("{:.2f}, {}".format(temp, count[i]))
         temp += .05
